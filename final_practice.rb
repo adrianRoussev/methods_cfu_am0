3894bd8c8433ca06d492e9ccc65b01a4e01c37ea
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-13 18:09:22 -0400

    add answers to final practice

--- final_practice.rb
+++ final_practice.rb
@@ -5,31 +5,47 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-
+def greeting
+    "greetings"
+end
+greeting
+# What is the return value of your method? "greetings"
+# How many arguments did you pass your method? 0
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def custom_greeting(name)
+    "greetings #{name}"
+end
+custom_greeting("adrian")
+# What is the return value of your method? "greetings adrian"
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+ef greet_person(first, middle, last)
+  "greetings #{first} is #{first} #{middle} #{last} your correct full name?"
+end
+greet_person("adrian", "T", "roussev")
+
+# What is the return value of your method? "greetings adrian is adrian T roussev your correct full name?"
+# How many arguments did you pass your method? 3
+# What data type was your argument(s)? string
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def square(n)
+    "the square of #{n} is #{n**2}"
+ end
+ p square(11)
+
+# What is the return value of your method? "the square of 11 is 121"
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -46,3 +62,14 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+def check_stock(n, item)
+    if n>=4
+    " #{item} is stocked"
+    elsif n == 0
+      "#{item} - OUT of stock"
+    else
+      "#{item} -  running LOW"
+   end
+  end
+   p check_stock(4, "coffee")
